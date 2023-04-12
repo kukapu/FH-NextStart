44e30b447e5820edd14e39a4e302d21cbf1f75aa
Albert Alvarez Estelles <susokukapu@gmail.com>
2023-04-12 11:23:32 +0200

    feat: add 02 pokemon staick with theme

---
+++ 02-pokemon-static/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@nextui-org/react";
+
+export default function Home() {
+  return (
+    <>
+      <Button color={"gradient"}>
+        Hola Mundo
+      </Button>
+    </>
+  )
+}
